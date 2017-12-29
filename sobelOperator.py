d1bf716df48a2ca80bcf401c3f68f6cfad6e3586
Altron001 <diegoramirez194@gmail.com>
2017-12-29 15:20:04 -0500

    Add files via upload
    First commit

---
+++ sobelOperator.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+def sobelOperator(img):
+    container = np.copy(img)
+    size = container.shape
+    for i in range(1, size[0] - 1):
+        for j in range(1, size[1] - 1):
+            gx = (img[i - 1][j - 1] + 2*img[i][j - 1] + img[i + 1][j - 1]) - (img[i - 1][j + 1] + 2*img[i][j + 1] + img[i + 1][j + 1])
+            gy = (img[i - 1][j - 1] + 2*img[i - 1][j] + img[i - 1][j + 1]) - (img[i + 1][j - 1] + 2*img[i + 1][j] + img[i + 1][j + 1])
+            container[i][j] = min(255, np.sqrt(gx**2 + gy**2))
+    return container
+    pass
+
+
+img = cv2.cvtColor(cv2.imread("sol_key.png"), cv2.COLOR_BGR2GRAY)
+img = sobelOperator(img)
+img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+plt.imshow(img)
+plt.show()
